f7f4fa4a13483f361aac09e915575d4809c9fa83
Aaron Ault <aultac@gmail.com>
2019-12-26 15:19:32 -0500

    Updates

---
+++ precache-manifest.dff6ceb8e0d1b6a195a2754a2b47c12b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7fe74cfc7640aae8cf5582707e5d234",
+    "url": "/invoices/index.html"
+  },
+  {
+    "revision": "8948b58f8050a4bca62e",
+    "url": "/invoices/static/css/main.5d3e723f.chunk.css"
+  },
+  {
+    "revision": "6107f89e93b5b367f124",
+    "url": "/invoices/static/js/2.4069d0a4.chunk.js"
+  },
+  {
+    "revision": "fea213033b0341efeb9835c89033908e",
+    "url": "/invoices/static/js/2.4069d0a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8948b58f8050a4bca62e",
+    "url": "/invoices/static/js/main.b4d3d1ba.chunk.js"
+  },
+  {
+    "revision": "96286cd8e9e2f15eec93",
+    "url": "/invoices/static/js/runtime-main.3bc909e9.js"
+  }
+]);
